9408fa7dd894da9a8068d6c022710dc9a16fb63a
MIKE01 <mikelutsik@gmail.com>
2020-11-05 11:42:48 -0800

    Made a few changes. Made a class to getTitle, getYear, and getCategory. It is outputting the data from the last line in the csv file. Still working on it to output the info we need. Just wanted to give a little update on whats going on.

--- src/main/java/api/Fetch.java
+++ src/main/java/api/Fetch.java
@@ -3,63 +3,77 @@ package api;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Fetch
 {
+    String[] movie;
 
-    private String csvFile = "src/main/CSVFile/KaggleData_the_oscar_award.csv";
-    private String data = "";
-    private List<String> newData = new ArrayList<>();
-    private List<String> line;
-
-
-    public String parseData() {
+    public void fetch() {
+        String csvFile = "src/main/CSVFile/KaggleData_the_oscar_award.csv";
+        String data = "";
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
             while ((data = br.readLine()) != null) {
                 // use comma as separator
-                String[] movie = data.split(",");
-                for(String token:movie)
-                {
-                    System.out.print(token + " | ");
-                }
-                System.out.println(" ");
+                movie = data.split(",");
             }
         }
-        catch (IOException e) {
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
-        return csvFile;
+
     }
+    
 
-
-    public List<String> newLine(int element)
+    public String getYear()
     {
-        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
-            while ((data = br.readLine()) != null) {
-                // use comma as separator
-                String[] movie = data.split(",");
-                //System.out.print(token + " | ");
-                newData.add(data);
-            }
-            line = Collections.singletonList(newData.get(element));
-
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return line;
+        return movie[0];
     }
+
+    public String getCategory()
+    {
+        return movie[3];
+    }
+
+    public String getTitle()
+    {
+        return movie[4];
+    }
+
+
 
     public static void main(String[] args) throws Exception
     {
         Fetch movie = new Fetch();
+        movie.fetch();
 
-        System.out.println((movie.newLine(7)));
+        System.out.println((movie.getYear()));
+        System.out.println((movie.getCategory()));
+        System.out.println((movie.getTitle()));
     }
 }
+
+
+/*
+        Scanner scan = new Scanner(System.in); //Creates a new scanner
+
+        System.out.println("Year: ");
+
+        String input = scan.nextLine();
+
+        System.out.println("Nomination: ");
+
+        String output = scan.nextLine();
+
+        for(String i:movie.newLine())
+        {
+            //System.out.println(exp.get(6));
+            if (i.contains(input) && i.contains(output))
+            {
+                System.out.println(i);
+            }
+        }
+ */
