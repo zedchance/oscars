4ff76dd5d26041875457a03613096c7cabfde7c8
MIKE01 <mikelutsik@gmail.com>
2020-11-05 18:52:07 -0800

    Rebuilt Fetch.java to get year, title, and ceremony and store it into a movie object array. Added a movie object as well.

--- src/main/java/api/Fetch.java
+++ src/main/java/api/Fetch.java
@@ -7,73 +7,39 @@ import java.util.*;
 
 public class Fetch
 {
-    String[] movie;
 
-    public void fetch() {
+    public static ArrayList<Movie> all() {
         String csvFile = "src/main/CSVFile/KaggleData_the_oscar_award.csv";
         String data = "";
+        String title;
+        String year;
+        String ceremony;
+        ArrayList<Movie> array = new ArrayList<Movie>();
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
+            br.readLine(); //reads the first line of all the headers because we are not interested in it.
             while ((data = br.readLine()) != null) {
                 // use comma as separator
-                movie = data.split(",");
+                String[] movie = data.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)", -1);
+
+                title = movie[5];
+                year = movie[0];
+                ceremony = movie[2];
+
+                array.add(new Movie(year, title, ceremony));
             }
+            System.out.println(array);
         }
         catch (IOException e)
         {
             e.printStackTrace();
         }
-
+        return array;
     }
-    
-
-    public String getYear()
-    {
-        return movie[0];
-    }
-
-    public String getCategory()
-    {
-        return movie[3];
-    }
-
-    public String getTitle()
-    {
-        return movie[4];
-    }
-
 
 
     public static void main(String[] args) throws Exception
     {
-        Fetch movie = new Fetch();
-        movie.fetch();
-
-        System.out.println((movie.getYear()));
-        System.out.println((movie.getCategory()));
-        System.out.println((movie.getTitle()));
+        all();
     }
 }
-
-
-/*
-        Scanner scan = new Scanner(System.in); //Creates a new scanner
-
-        System.out.println("Year: ");
-
-        String input = scan.nextLine();
-
-        System.out.println("Nomination: ");
-
-        String output = scan.nextLine();
-
-        for(String i:movie.newLine())
-        {
-            //System.out.println(exp.get(6));
-            if (i.contains(input) && i.contains(output))
-            {
-                System.out.println(i);
-            }
-        }
- */
---
+++ src/main/java/api/Movie.java
@@ -0,0 +1,31 @@
+package api;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Movie
+{
+    private String title;
+    private String year;
+    private String ceremony;
+
+    public Movie(String year, String title, String ceremony)
+    {
+        this.title = title;
+        this.year = year;
+        this.ceremony = ceremony;
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Movie{" +
+                "title='" + title + '\'' +
+                ", year='" + year + '\'' +
+                ", ceremony='" + ceremony + '\'' +
+                '}';
+    }
+}
