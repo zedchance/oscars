72b467de114a3e9ab602b4ea8e3a9c6e9f6ef25a
Visoth <blacksterteoz@gmail.com>
2020-11-14 15:48:19 -0800

    - create in the table database to which store the movie objects -search through the movie objects by name -"problem": the duplicate movies in the output because it has difference values of the awards - the output is the Json format

---
+++ src/main/java/api/data.java
@@ -0,0 +1,51 @@
+package api;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+//connect the SQL database file with java
+public class data {
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:movies.sqlite";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+    //----------------end of the connect method-----------------------
+    //select a specific field in the SQL table
+    public void selectAll(){
+        //input to get specific data from the table
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the movie name: ");
+
+        String mo = "'"+myObj.nextLine()+"%'";
+        String sql = "SELECT json FROM movies WHERE title LIKE "+mo;
+
+        try (Connection conn = this.connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            // loop through the result set
+            while (rs.next()) {
+                System.out.println(rs.getString("json") +  "\t" );
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    //the main method to run the test
+    public static void main(String[] args) {
+        data app = new data();
+        app.selectAll();
+    }
+
+}
