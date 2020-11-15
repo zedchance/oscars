ca0845037eefd721dbd8e74c96d8f5cfc97f8359
Zed Chance <zchance@csus.edu>
2020-11-14 20:47:29 -0800

    Add optional winner parameter to /category endpoint
    This can be used to narrow down the category results to movies that have won, or lost.
    For example: localhost:8080/category/ACTRESS?winner=true or
    http://localhost:8080/category/MUSIC?winner=false

--- src/main/java/api/Controller.java
+++ src/main/java/api/Controller.java
@@ -55,20 +55,24 @@ public class Controller implements ErrorController
      * in the wiki.
      *
      * @param category category to search for
+     * @param winner optional boolean to specify if award was won or not
      * @return an ArrayList<Movie> of all movies containing award category
      */
     @GetMapping("/category/{category}")
-    public ArrayList<Movie> category(@PathVariable("category") String category)
+    public ArrayList<Movie> category(@PathVariable("category") String category,
+                                     @RequestParam(value = "winner", defaultValue = "none") String winner)
     {
         ArrayList<Movie> all = FetchFromCSV.all();
         ArrayList<Movie> matches = new ArrayList<>();
-        for (Movie movie: all)
+        for (Movie movie : all)
         {
-            for (Award award: movie.getAwards())
+            for (Award award : movie.getAwards())
             {
                 if (award.getCategory().contains(category))
                 {
-                    matches.add(movie);
+                    if ("true".equalsIgnoreCase(winner) && award.isWinner()) matches.add(movie);
+                    else if ("false".equalsIgnoreCase(winner) && !award.isWinner()) matches.add(movie);
+                    else if ("none".equals(winner)) matches.add(movie);
                 }
             }
         }
