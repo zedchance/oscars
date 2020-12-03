ab82902fd085b1ced4b7ea7b30f126bb71436321
Victor Galbraith <victorg87@gmail.com>
2020-12-03 10:43:09 -0800

    changed order on modifiers to comply with Java standards (Was brought up on SonarQube before)

--- src/main/java/api/APIKeys.java
+++ src/main/java/api/APIKeys.java
@@ -2,8 +2,8 @@ package api;
 
 class APIKeys
 {
-    final static String OMDB_ID = "e927bb71";
-    final static String TMDB_ID = "753457d6baec682a399fec89c1d310a7";
+    static final String OMDB_ID = "e927bb71";
+    static final String TMDB_ID = "753457d6baec682a399fec89c1d310a7";
 
     private APIKeys()
     {
